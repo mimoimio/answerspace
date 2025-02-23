fb9525d89839328b51464a9cd5e2f074d89f4029
mimoimio <129141433+mimoimio@users.noreply.github.com>
2025-02-23 15:49:00 +0000

    sampai mana ni

--- index.php
+++ index.php
@@ -1,1 +1,65 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="styles.css">
+    <title>Mior's AnswerSpace</title>
+
+</head>
+
+<body>
+    <div class="container mx-auto flex flex-col p-4">
+        <h1 class="text-[2rem] drop-shadow-[0_0_4rem_rgba(1,1,1,0.5)] text-center text-white tracking-[.5rem]">
+            AnswerSpace</h1>
+        <div class="flex flex-col lg:grid lg:grid-cols-3 gap-4">
+            <div class="answer-item gap-4">
+                <h2>Answer #1</h2>
+                <hr>
+                <p>Lorem Ipsum aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                    text-overflow: 'ellipsis';
+                    word-wrap: break-word;
+                    overflow:hidden;
+                    text-align: justify;
+                    text-overflow: 'ellipsis';
+                    word-wrap: break-word;
+                    overflow:hidden;
+                    text-align: justify;</p>
+            </div>
+            <div class="answer-item gap-4">
+                <h2>Answer #1</h2>
+                <hr>
+                <p>Lorem Ipsum aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                    text-overflow: 'ellipsis';
+                    word-wrap: break-word;
+                    overflow:hidden;
+                    text-align: justify;
+                    text-overflow: 'ellipsis';
+                    word-wrap: break-word;
+                    overflow:hidden;
+                    text-align: justify;</p>
+            </div>
+            <div class="answer-item gap-4">
+                <h2>Answer #1</h2>
+                <hr>
+                <p>Lorem Ipsum aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                    text-overflow: 'ellipsis';
+                    word-wrap: break-word;
+                    overflow:hidden;
+                    text-align: justify;
+                    text-overflow: 'ellipsis';
+                    word-wrap: break-word;
+                    overflow:hidden;
+                    text-align: justify;</p>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
